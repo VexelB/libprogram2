14ed76bda34fd39ff16ff7730fe8cbd5f702b12c
Voytin Evgeny <vexelb@gmail.com>
2020-10-20 14:58:09 +0300

    some

--- serv.js
+++ serv.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = new express();
 const path = require("path")
+const { Server } = require('ws');
+const wss = new Server({ port: 5354 });
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 
-app.listen(5353, () => {
-    console.log('listen')
+app.listen(5353, () => {})
+wss.on('connection', (ws, req) => {
+    ws.send('hi');
 })
