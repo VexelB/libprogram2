e793b32c2491495b17f316cc793401e84fa75fb3
vexelb <vexelb@gmail.com>
2020-10-21 19:26:21 +0300

    ьвф

--- serv.js
+++ serv.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = new express();
 const path = require("path")
+const sqlite3 = require("sqlite3")
 const { Server } = require('ws');
 const wss = new Server({ port: 5354 });
 const password = 'random';
@@ -24,6 +25,20 @@ wss.on('connection', (ws, req) => {
                 clients.push(req.connection.remoteAddress);
                 setTimeout(() => {clients.splice(clients.indexOf(req.connection.remoteAddress, 1))}, 600000)
             }
+        }
+        if (d.action == "get") {
+            let db = new sqlite3.Database('sqlite.db', sqlite3.OPEN_READWRITE, (err) => {
+                if (err) {
+                  console.error(err.message);
+                }
+            });
+            let answer = [];
+            db.serialize(() => {
+                db.all(`select * from ${d.table}`, (err,rows) => {
+                    ws.send(JSON.stringify(rows))
+                })
+            })
+            
         }
     })
 })
