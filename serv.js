959cf79354ae6cd2715c3f2edff1e092131cdea5
Voytin Evgeny <vexelb@gmail.com>
2020-10-21 13:11:40 +0300

    12

--- serv.js
+++ serv.js
@@ -8,17 +8,16 @@ const password = 'adminka';
 app.get('/*', (req, res) => {
     if (req.query.pass == password) {
         res.sendFile(path.join(__dirname, 'index.html'))
-        
     }
     else {
         res.sendFile(path.join(__dirname, 'login.html'));
     }
 })
 
-
 app.listen(5353, () => {})
 wss.on('connection', (ws, req) => {
+    console.log('ura')
     ws.on('message', (d) => {
-        
+        let temp = JSON.parse(d);
     })
 })
